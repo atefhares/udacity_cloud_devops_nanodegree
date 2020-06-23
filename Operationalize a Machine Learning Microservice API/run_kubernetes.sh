f7db332af3e295241ded3601dc3055d1f6192f0e
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-06-23 18:21:13 +0200

    task 5: deploy with kubernetes

--- Operationalize a Machine Learning Microservice API/run_kubernetes.sh
+++ Operationalize a Machine Learning Microservice API/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=atefhares/udacity:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=udacity_python_app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment python_app --type=LoadBalancer --port=80 --target-port=80
