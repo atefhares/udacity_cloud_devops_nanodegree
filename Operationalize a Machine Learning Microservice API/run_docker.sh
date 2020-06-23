53f1638bca752382f9077a9fb9bbb2d6726508db
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-06-23 17:47:50 +0200

    task 2: Run Docker container

--- Operationalize a Machine Learning Microservice API/run_docker.sh
+++ Operationalize a Machine Learning Microservice API/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=udacity_python_app . 
 
 # Step 2: 
 # List docker images
+docker images ls
 
 # Step 3: 
 # Run flask app
+docker run -p 80:80 udacity_python_app
